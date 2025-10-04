50695a6ff8568da3da8596b794d8bbabe3044abc
iannonas <iannonas@gmail.com>
2025-10-04 16:37:29 +0800

    Completed Member D tasks

---
+++ ShoeInventoryApp/Program.cs
@@ -0,0 +1,31 @@
+using ShoeInventoryApp.Models;
+using ShoeInventoryApp.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<IShoeService, ShoeService>();
+builder.Services.AddScoped<IPurchaseOrderService, PurchaseOrderService>();
+builder.Services.AddScoped<ISupplierService, SupplierService>();
+builder.Services.AddScoped<IShoeColorVariationService, ShoeColorVariationService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
